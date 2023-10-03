9a1e62c51be247e408faa0888925587ab1714529
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-10-03 22:38:06 +0500

    Classwork3

--- Classwork3.cs
+++ Classwork3.cs
@@ -11,29 +11,26 @@ namespace ConsoleApp27
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите стороны прямоугольника через пробел");
-            string x = Console.ReadLine();
-            x = x.Trim();
-            string[] rez;
-            rez = x.Split(' ');
-            int x4 = x.Length;
-            int x1;
-            int x2;
-            bool p = int.TryParse(rez[0], out x1);
-            bool p1 = int.TryParse(rez[1], out x2);
-            int x3 = x1 * x2;
-            Console.WriteLine(x4);
-            Console.WriteLine(x1);
-            Console.WriteLine(x2);
-            if (x4 <= 3 && p == true && p1 == true)
-            {
-                Console.WriteLine("Площадь: " + x3);
-            }
-            else
-            {
-                Console.WriteLine("Ошибка");
-            }
-            Console.ReadKey();
+           Console.WriteLine("Введите стороны прямоугольника через пробел");
+           string x = Console.ReadLine();
+           x = x.Trim();
+           string[] rez;
+           rez = x.Split(' ');
+           int x4 = x.Length;
+           int x1;
+           int x2;
+           bool p = int.TryParse(rez[0], out x1);
+           bool p1 = int.TryParse(rez[1], out x2);
+           int x3 = x1 * x2;
+           if (x4 <= 3 && p == true && p1 == true)
+           {
+               Console.WriteLine("Площадь: " + x3);
+           }
+           else
+           {
+               Console.WriteLine("Ошибка");
+           }
+           Console.ReadKey();
 
         }
     }
